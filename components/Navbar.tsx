95da7c1dc8c5f7893361223b616ce934f3da619a
incogiscool <adam546944@gmail.com>
2022-10-27 20:10:05 +0400

    update nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,9 @@
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center w-full fixed z-[1] top-0 shadow-lg p-3 backdrop-blur-sm">
-      <h1 className="font-bold text-xl">EXPONENTIAL STUDIOS</h1>
+      <h1 className="font-bold sm:text-xl text-sm">EXPONENTIAL STUDIOS</h1>
       <a href="https://calendly.com/exponentialstudios/initial-call">
-        <div className="text-lg bg-white pr-8 pl-8 pt-2 pb-2 rounded-md text-black font-medium flex items-cente hover:bg-slate-100 transition">
+        <div className="sm:text-lg text-md bg-white pr-8 pl-8 pt-2 pb-2 rounded-md text-black font-medium flex items-cente hover:bg-slate-100 transition">
           Book a Call
           <div className="ml-2">
             <svg
